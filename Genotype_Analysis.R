e95a01f37212a75cbe7cf89271ee9776d9b609d7
AHuffmyer <ashuff@hawaii.edu>
2020-02-06 15:51:20 -1000

    Updating figures

--- Genotype_Analysis.R
+++ Genotype_Analysis.R
@@ -1,6 +1,6 @@
 setwd("Data/"); library(tidyverse);library(readxl);library(ggdendro);library(ape);library(SNPRelate)
 
-#read IBS matrix
+#read IBS matrix from FIRST analysis: no pvalue cut off 
 name <- "angsd.IBS.ibsMat"
 m <- as.matrix(read.table(name))
 
@@ -23,7 +23,4 @@ plot(ape::nj(m))
 
 plot(hclust(dist(m), "complete"))
 abline(h=0.01485, col="blue") #genetic distance b/t SM29 and SM41
-
-
-
 
